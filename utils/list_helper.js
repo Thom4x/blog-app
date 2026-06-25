1be2bf48331fd8ab926f17fa719651d09050c79d
Thom4x <tomhoser.2108@hotmail.com>
2026-06-25 15:15:16 -0500

    4.5 test blogs 1

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,8 +9,23 @@ const totalLikes = (likedBlog) => {
     return liked.reduce((sum, likes) => sum + likes, 0)
 }
 
+const favoriteBlog = (blogLiked) => {
+    if (blogLiked.length === 0) return null;
+
+    const maxLiked = Math.max(...blogLiked.map((user) => user.likes))
+    const favorite = blogLiked.find(max => max.likes === maxLiked)
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+}
+
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
 
