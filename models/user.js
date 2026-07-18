5cefd727e41e60b1c12b09cb9464ed1ca5190423
Thom4x <tomhoser.2108@hotmail.com>
2026-07-18 12:06:25 -0500

    4.15 bcrypt blog list

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const { min } = require('lodash')
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+// Conectar
+mongoose.connect(config.MONGODB_URI)
+
+// Crear esquema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        min: 3
+    },
+    name: String,
+    passwordHash: String,
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ],
+})
+
+// Transformar el objeto a JSON
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        // el passwordHash no debe mostrarse
+        delete returnedObject.passwordHash
+    }
+})
+
+// Crear modelo
+module.exports = mongoose.model('User', userSchema)
