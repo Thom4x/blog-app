0da675b331f8eaea4db7070261db4173f2dbc05a
Thom4x <tomhoser.2108@hotmail.com>
2026-08-12 17:57:56 -0500

    5.15 Blog vitest

--- frontend/src/components/Blog.test.jsx
+++ frontend/src/components/Blog.test.jsx
@@ -38,14 +38,42 @@ test('Al hacer click en ver, se muestra mas informacion', async () => {
 
     render(<Blog blog={blog} />)
 
-    screen.debug()
+    //screen.debug()
 
     const user = userEvent.setup()
     const button = screen.getByTestId('view')
     await user.click(button)
 
-    screen.debug()
+    //screen.debug()
 
     const element = screen.getByTestId('blog-details')
     expect(element).toHaveTextContent('https://www.youtube.com/watch?v=6p6PcFFUm5I')
+})
+test('Al hacer click en like, se llama al controlador de eventos 2 veces', async () => {
+    const blog = {
+        title: 'OVOSOUND-ICEMAN',
+        author: 'Drake',
+        url: 'https://www.youtube.com/watch?v=6p6PcFFUm5I',
+        likes: 1000,
+        user: {
+            username: 'drake',
+            name: 'Drake'
+        }
+    }
+
+    const mockLikeHandler = vi.fn()
+
+    render(<Blog blog={blog} updateLikes={mockLikeHandler} />)
+
+
+    const user = userEvent.setup()
+    const button = screen.getByTestId('view')
+    await user.click(button)
+
+    // aqui se hace click en el boton like dos veces
+    const button2 = screen.getAllByText('like')
+    await user.click(button2[0])
+    await user.click(button2[0])
+
+    expect(mockLikeHandler.mock.calls).toHaveLength(2)
 })
