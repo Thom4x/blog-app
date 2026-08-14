74963480562b76851557d1d6033c8b7a0d5ceaff
Thom4x <tomhoser.2108@hotmail.com>
2026-08-14 17:14:29 -0500

    5.19 E2E Blog 3

--- e2e/tests/blogtest.spec.js
+++ e2e/tests/blogtest.spec.js
@@ -36,6 +36,23 @@ describe('Blog App', () => {
             const errorDiv = page.locator('.error')
             await expect(errorDiv).toContainText('Invalid username or password')
         })
+        describe('When logged in', () => {
+            beforeEach(async ({ page }) => {
+                await page.getByRole('button', { name: 'login' }).click()
+                await page.getByLabel('username').fill('lamine')
+                await page.getByLabel('password').fill('a123456')
+                await page.getByRole('button', { name: 'Login' }).click()
+            })
+            test('Blog can be created', async ({ page }) => {
+                await page.getByRole('button', { name: 'create blog' }).click()
+                await page.getByLabel('title:').fill('Skills 2 with LAMINE')
+                await page.getByLabel('author:').fill('Lamine Yamal')
+                await page.getByLabel('url:').fill('www.ytlamine.con')
+                await page.getByRole('button', { name: 'Create' }).click()
 
+                const successBlog = page.getByTestId('blog')
+                await expect(successBlog.filter({ hasText: 'Skills 2 with LAMINE' })).toBeVisible()
+            })
+        })
     })
 })
